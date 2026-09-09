07269c6355e45df9d602fb0d91ce89ad92649089
ved11k <mrunal11k@gmail.com>
2026-09-09 14:14:40 +0530

    Updates in files

--- docusaurusfirstsite/docusaurus.config.js
+++ docusaurusfirstsite/docusaurus.config.js
@@ -10,8 +10,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Banking Software Documentation',
+  tagline: 'User Guide for Banking Software',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
@@ -20,15 +20,15 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
+  url: 'https://ved11k.github.io',
+  // Set the /docasaurus-training// pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/docasaurus-training/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'ved11k', // Usually your GitHub org/user name.
+  projectName: 'docasaurus-training', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
